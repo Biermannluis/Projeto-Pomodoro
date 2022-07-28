8ee2eaa5df2c87050563d78f80c8a46bcb025b66
Luis Biermann <eng.biermannluis@gmail.com>
2022-07-28 09:18:02 -0300

    Reposicao diaria

--- cronometro_seg.js
+++ cronometro_seg.js
@@ -19,6 +19,9 @@ function pause_seg() {
 function stop_seg() {
     clearInterval(interval_seg);
     document.getElementById('div_seg').innerHTML = "00"
+    document.getElementById('div_min').innerHTML = "00"
+    document.getElementById('div_hor').innerHTML = "00"
+
     seg = 0
     min = 0
     hor = 0
@@ -27,75 +30,84 @@ function stop_seg() {
 function cont_seg() {
     if (min < set_work_time) {
         seg++
+        console.log('seg')
+
         idtimer.style.color = 'red';
-    }
+        document.getElementById('div_seg').innerHTML = "00"
+        document.getElementById('div_min').innerHTML = "00"
+        document.getElementById('div_hor').innerHTML = "00"
+        min_chill = 0
+        seg_chill = 0
+        hor_chill = 0
 
-    if (seg <= 9) {
-        document.getElementById('div_seg').innerHTML = "0" + seg
-    }
-    else {
-        document.getElementById('div_seg').innerHTML = seg
-    }
 
-    if (seg == 60) {
-        min++
-        seg = 0
-        if (min <= 9) {
-            document.getElementById('div_min').innerHTML = "0" + min
+        if (seg <= 9) {
+            document.getElementById('div_seg').innerHTML = "0" + seg
         }
         else {
-            document.getElementById('div_min').innerHTML = min
+            document.getElementById('div_seg').innerHTML = seg
         }
-    }
-    if (min == 6) {
-        hor++
-        min = 0
-        if (hor <= 9) {
-            document.getElementById('div_hor').innerHTML = "0" + hor
+
+        if (seg == 60) {
+            min++
+            console.log('min')
+            seg = 0
+            if (min <= 9) {
+                document.getElementById('div_min').innerHTML = "0" + min
+            }
+            else {
+                document.getElementById('div_min').innerHTML = min
+            }
         }
-        else {
-            document.getElementById('div_hor').innerHTML = hor
+        if (min == 6) {
+            hor++
+            min = 0
+            if (hor <= 9) {
+                document.getElementById('div_hor').innerHTML = "0" + hor
+            }
+            else {
+                document.getElementById('div_hor').innerHTML = hor
+            }
         }
+
     }
     //=======================CHILL===============================================
-    if (min_chill < set_chill_time & min>=set_work_time) {
+    if (min_chill < set_chill_time & min >= set_work_time) {
         seg_chill++
         idtimer.style.color = 'blue';
-    }else{
-        seg=0
-        min=0
-        hor=0
-        seg_chill=0
-        min_chill=0
-        hor_chill=0    
-    }
+        document.getElementById('div_seg').innerHTML = "00"
+        document.getElementById('div_min').innerHTML = "00"
+        document.getElementById('div_hor').innerHTML = "00"
+        
 
 
-    if (seg_chill <= 9) {
-        document.getElementById('div_seg').innerHTML = "0" + seg_chill
-    }
-    else {
-        document.getElementById('div_seg').innerHTML = seg_chill
-    }
 
-    if (seg_chill == 60) {
-        min_chilln++
-        seg_chillg = 0
-        if (min_chilln <= 9) {
-            document.getElementById('div_min').innerHTML = "0" + min_chill
+        if (seg_chill <= 9) {
+            document.getElementById('div_seg').innerHTML = "0" + seg_chill
         }
         else {
-            document.getElementById('div_min').innerHTML = min_chill
+            document.getElementById('div_seg').innerHTML = seg_chill
         }
-    }
-    if (min_chill == 6) {
-        hor_chill++
-        min_chill = 0
-        if (hor_chill <= 9) {
-            document.getElementById('div_hor').innerHTML = "0" + hor_chill
+
+        if (seg_chill == 60) {
+            min_chill++
+            seg_chill = 0
+            if (min_chill <= 9) {
+                document.getElementById('div_min').innerHTML = "0" + min_chill
+            }
+            else {
+                document.getElementById('div_min').innerHTML = min_chill
+            }
         }
-        else {
-            document.getElementById('div_hor').innerHTML = hor_chill
+        if (min_chill == 6) {
+            hor_chill++
+            min_chill = 0
+            if (hor_chill <= 9) {
+                document.getElementById('div_hor').innerHTML = "0" + hor_chill
+            }
+            else {
+                document.getElementById('div_hor').innerHTML = hor_chill
+            }
         }
     }
 }
